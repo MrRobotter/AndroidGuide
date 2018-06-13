af4abbfa0ab2e280f7837a4c74f9731fd1f446e8
zhangjunyang <2816886869@qq.com>
2018-06-13 19:36:08 +0800

    修改

--- app/src/main/java/com/joinyon/androidguide/MyView.java
+++ app/src/main/java/com/joinyon/androidguide/MyView.java
@@ -1,9 +1,22 @@
 package com.joinyon.androidguide;
 
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
 /**
  * 作者： JoinYon on 2018/6/13.
  * 邮箱：2816886869@qq.com
  */
 
-public class MyView {
+public class MyView extends View{
+
+    public MyView(Context context) {
+        super(context);
+    }
+
+    public MyView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
 }
---
+++ resource/Android/自定义View.md
@@ -0,0 +1,19 @@
+# 1 自定义View
+
+## 1.1 onMeasure
+
+## 1.2 重写onMeasure方法
+
+## 1.3 重写onDraw方法
+
+## 1.4 自定义布局属性
+
+# 2 自定义ViewGroup
+
+## 2.1 重写onMeasure方法
+
+## 2.2 要实现的功能
+
+## 2.3 子view的摆放规则
+
+## 2.4 摆放子view
