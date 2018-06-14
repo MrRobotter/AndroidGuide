368151449e400772a9925d03663e29c8c5e3fc92
zhangjunyang <2816886869@qq.com>
2018-06-14 18:36:01 +0800

    合并分支

--- app/src/main/java/com/joinyon/androidguide/ImageSaveUtil.java
+++ app/src/main/java/com/joinyon/androidguide/ImageSaveUtil.java
@@ -1,9 +1,82 @@
 package com.joinyon.androidguide;
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 /**
  * 作者： JoinYon on 2018/6/14.
  * 邮箱：2816886869@qq.com
+ * 保存图片的工具
  */
 
 public class ImageSaveUtil {
+    public static boolean saveBitmap(ImageView view, String filePath) {
+        Drawable drawable = view.getDrawable();
+        if (drawable == null) {
+            return false;
+        }
+
+        FileOutputStream outputStream = null;
+        File file = new File(filePath);
+        if (file.isDirectory()) {
+            return false;
+        }
+        try {
+            outputStream = new FileOutputStream(file);
+            Bitmap bitmap = ((BitmapDrawable) drawable).getBitmap();
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
+            outputStream.flush();
+            outputStream.close();
+            return true;
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+//    public static boolean saveBitmap(View view, String filePath) {
+//
+//        // 创建对应大小的bitmap
+//        Bitmap bitmap = Bitmap.createBitmap(view.getWidth(), view.getHeight(),
+//                Bitmap.Config.ARGB_8888);
+//        Canvas canvas = new Canvas(bitmap);
+//        view.draw(canvas);
+//
+//        //存储
+//        FileOutputStream outStream = null;
+//        File file = new File(filePath);
+//        if (file.isDirectory()) {//如果是目录不允许保存
+//            return false;
+//        }
+//        try {
+//            outStream = new FileOutputStream(file);
+//            bitmap.compress(Bitmap.CompressFormat.PNG, 100, outStream);
+//            outStream.flush();
+//            return true;
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//            return false;
+//
+//        } finally {
+//            try {
+//                bitmap.recycle();
+//                if (outStream != null) {
+//                    outStream.close();
+//                }
+//
+//
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//    }
 }
--- app/src/main/java/com/joinyon/androidguide/MainActivity.java
+++ app/src/main/java/com/joinyon/androidguide/MainActivity.java
@@ -1,13 +1,121 @@
 package com.joinyon.androidguide;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity implements View.OnLongClickListener {
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageView = findViewById(R.id.imageView);
+        imageView.setOnLongClickListener(this);
+
+        Drawable drawable = imageView.getDrawable();
+        Log.e("AAA", drawable.toString());
+    }
+
+    @Override
+    public boolean onLongClick(View v) {
+        if (Build.VERSION.SDK_INT >= 23) {
+            int REQUEST_CODE_CONTACT = 101;
+            String[] permissions = {Manifest.permission.WRITE_EXTERNAL_STORAGE};
+            //验证是否许可权限
+            for (String str : permissions) {
+                if (this.checkSelfPermission(str) != PackageManager.PERMISSION_GRANTED) {
+                    //申请权限
+                    this.requestPermissions(permissions, REQUEST_CODE_CONTACT);
+                }
+            }
+        }
+//        String fileDir = Environment.DIRECTORY_PICTURES + "/" + "images/" + "戴鼠微信公众号.jpeg";
+//        if (ImageSaveUtil.saveBitmap(imageView, fileDir)) {
+//
+//            Toast.makeText(this, "图片以保存至" + fileDir, Toast.LENGTH_SHORT).show();
+//        } else {
+//            Toast.makeText(this, "图片保存失败！", Toast.LENGTH_SHORT).show();
+//        }
+        viewSaveToImage(imageView);
+
+        return false;
+    }
+
+    private void viewSaveToImage(View view) {
+        view.setDrawingCacheEnabled(true);
+        view.setDrawingCacheQuality(View.DRAWING_CACHE_QUALITY_HIGH);
+        view.setDrawingCacheBackgroundColor(Color.WHITE);
+
+        // 把一个View转换成图片
+        Bitmap cachebmp = loadBitmapFromView(view);
+
+        FileOutputStream fos;
+        String imagePath = "";
+        File file = null;
+        try {
+            // 判断手机设备是否有SD卡
+            boolean isHasSDCard = Environment.getExternalStorageState().equals(
+                    android.os.Environment.MEDIA_MOUNTED);
+            if (isHasSDCard) {
+                // SD卡根目录
+                File sdRoot = Environment.getExternalStorageDirectory();
+                file = new File(sdRoot, "戴鼠智能微信公众号.png");
+                fos = new FileOutputStream(file);
+                imagePath = file.getAbsolutePath();
+            } else
+                throw new Exception("创建文件失败!");
+
+            cachebmp.compress(Bitmap.CompressFormat.PNG, 90, fos);
+
+            fos.flush();
+            fos.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Log.e("imagePath=" + imagePath, "AAA");
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);//扫描单个文件
+        intent.setData(Uri.fromFile(file));//给图片的绝对路径
+        this.sendBroadcast(intent);
+
+        Toast.makeText(this, "保存成功！", Toast.LENGTH_SHORT).show();
+        view.destroyDrawingCache();
+    }
+
+    private Bitmap loadBitmapFromView(View v) {
+        int w = v.getWidth();
+        int h = v.getHeight();
+
+        Bitmap bmp = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        Canvas c = new Canvas(bmp);
+
+        c.drawColor(Color.WHITE);
+        /** 如果不设置canvas画布为白色，则生成透明 */
+
+        v.layout(0, 0, w, h);
+        v.draw(c);
+
+        return bmp;
     }
 }
