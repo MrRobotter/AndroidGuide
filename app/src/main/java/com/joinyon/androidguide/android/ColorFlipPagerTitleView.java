b11116948f683fa88912aad395970d56085e5149
zhangjunyang <2816886869@qq.com>
2018-07-09 18:28:52 +0800

    一个可自定义的导航栏

---
+++ app/src/main/java/com/joinyon/androidguide/android/ColorFlipPagerTitleView.java
@@ -0,0 +1,51 @@
+package com.joinyon.androidguide.android;
+
+import android.content.Context;
+
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.titles.SimplePagerTitleView;
+
+/**
+ * 作者： JoinYon on 2018/7/8.
+ * 邮箱：2816886869@qq.com
+ */
+public class ColorFlipPagerTitleView extends SimplePagerTitleView {
+    private float mChangePercent = 0.5f;
+
+    public ColorFlipPagerTitleView(Context context) {
+        super(context);
+    }
+
+    @Override
+    public void onLeave(int index, int totalCount, float leavePercent, boolean leftToRight) {
+        if (leavePercent >= mChangePercent) {
+            setTextColor(mNormalColor);
+        } else {
+            setTextColor(mSelectedColor);
+        }
+    }
+
+    @Override
+    public void onEnter(int index, int totalCount, float enterPercent, boolean leftToRight) {
+        if (enterPercent >= mChangePercent) {
+            setTextColor(mSelectedColor);
+        } else {
+            setTextColor(mNormalColor);
+        }
+    }
+
+    @Override
+    public void onSelected(int index, int totalCount) {
+    }
+
+    @Override
+    public void onDeselected(int index, int totalCount) {
+    }
+
+    public float getChangePercent() {
+        return mChangePercent;
+    }
+
+    public void setChangePercent(float changePercent) {
+        mChangePercent = changePercent;
+    }
+}
